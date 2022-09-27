c05b481d44856ff5ee827b13394be63aeb085431
md_rajib_rari <rajibhossen688@gmail.com>
2022-09-28 01:01:21 +0600

    Add

---
+++ C-Sharp/AbstractExample/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AbstractExample;
+
+var type = Console.ReadLine();
+
+
+Vehicle Vehicle = GetVehicle(type);
+Vehicle.Color = "Red";
+Vehicle.Weight = 200;
+Vehicle.Start();
+Vehicle.Wash();
+
+Vehicle GetVehicle(string type)
+{
+	if (type=="Car")
+	{
+		return new Car();
+	}
+	else
+	{
+		return new Plane();
+	}
+}
---
+++ C-Sharp/AbstractExample/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample
+{
+    public abstract class Vehicle
+    {
+        public double Speed { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+        public int PassengerCount { get; set; }
+
+        public Vehicle()
+        {
+            Color = "Blue";
+        }
+
+        public virtual void Wash()
+        {
+            Console.WriteLine("Washing");
+        }
+
+        public abstract void Start();
+
+        public abstract void stop();
+    }
+}
