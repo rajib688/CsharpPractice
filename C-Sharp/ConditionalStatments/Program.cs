b2d87760aa1e2933eb79b903b235800dc84c5d18
md_rajib_rari <rajibhossen688@gmail.com>
2022-09-28 00:51:58 +0600

    Add

---
+++ C-Sharp/ConditionalStatments/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = 5;
+
+if (x > 5)
+{
+    Console.WriteLine("X Is smaller than 4");
+}
+else if (x < 10)
+{
+    Console.WriteLine("X is Greater than 10");
+}
+else
+{
+    Console.WriteLine("X Is equal to 5");
+}
+
+
+string message = x > 5 ? "x is greater than 5" : "x is not greater than 5";
+
+Console.WriteLine(message);
