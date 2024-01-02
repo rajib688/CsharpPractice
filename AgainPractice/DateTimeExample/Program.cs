3e109d0b3ed8172ed7be6cba16d740b664945abf
md_rajib_rari <rajibhossen688@gmail.com>
2024-01-02 23:42:35 +0600

    StringBuilder

---
+++ AgainPractice/DateTimeExample/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+
+DateTime dateTime = new DateTime(2024,6,27,10,40, 10);
+Console.WriteLine(dateTime);
+Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.UtcNow);
+
+/*
+string Line = Console.ReadLine();
+
+string[] parts = Line.Split(' ');
+int x = int.Parse(parts[0]);
+int y = int.Parse(parts[1]);
+int z = int.Parse(parts[2]);
+*/
+
+
+DateTime time = DateTime.Parse("2022/06/27");
+
+Console.WriteLine(time.DayOfWeek);
---
+++ AgainPractice/StringBuilderExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+string txt = "Hello World";
+
+txt = txt + " From C#";
+Console.WriteLine(txt);
+
+
+StringBuilder sb = new StringBuilder();
+sb.Append(txt);
+sb.AppendLine(" From C#");
+sb[2] = 'M';
