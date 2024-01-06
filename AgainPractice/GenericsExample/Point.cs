a2398f070f3a0711e981d765ec4f3219d993336e
md_rajib_rari <rajibhossen688@gmail.com>
2024-01-06 18:32:15 +0600

    Generics

---
+++ AgainPractice/GenericsExample/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    public class Point<T, V>
+    {
+        public T x { get; set; }
+        public V y { get; set; }
+    }
+    
+    public class SuperPoint<A, B, C> : Point<A, B>
+    {
+        public C Z { get; set; }
+    }
+
+
+    //public class Point<T>
+    //{
+    //    public T x { get; set; }
+    //    public T y { get; set; }
+    //}
+
+}
---
+++ AgainPractice/GenericsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GenericsExample;
+
+Console.WriteLine("Hello, World!");
+
+Point<int, double> point = new Point<int, double>();
+Point<int, int> point1 = new Point<int, int>();
+Point<double, int> point2 = new Point<double, int>();
+
+
+point.x = 1;
+point.y = 2.5;
+
+SuperPoint<int, string> superPoint = new SuperPoint<int, string>();
+superPoint.x = 5;
+superPoint.y = "Rajib Rari";
